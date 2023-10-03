f2da376c78c54a6420452bdcbea713fa11e93f88
callmegrisha <callmegrisha@gmail.com>
2023-10-03 18:18:21 +0300

    make prettier script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const myTreeView = document.querySelector(".tree-view");
+  const myTreeView = document.querySelector('.tree-view');
 
   initializeTreeView(myTreeView);
   toggleTreeViews(myTreeView);
@@ -11,31 +11,26 @@ function hasClass(element, className) {
 
 function initializeTreeView(element) {
   const folders = element.querySelectorAll('.folder');
-  folders.forEach(folder => {
+  for (let folder of folders) {
     const caretBtn = folder.querySelector('.caret__btn');
     if (folder.classList.contains('folder--opened')) {
       caretBtn.textContent = '-';
     } else {
       caretBtn.textContent = '+';
     }
-  });
+  }
 }
 
 function toggleTreeViews(element) {
-  element.addEventListener("click", (e) => {
-      let target = e.target;
-      
-      if (hasClass(target, "caret__btn")) {
-        const folder = target.closest(".folder");
-        target.parentNode.classList.toggle("caret--opened");
-        folder && folder.classList.toggle("folder--opened");
+  element.addEventListener('click', (e) => {
+    let target = e.target;
 
-        target.textContent = target.textContent === "+" ? "-" : "+";
-      }
+    if (hasClass(target, 'caret__btn')) {
+      const folder = target.closest('.folder');
+      target.parentNode.classList.toggle('caret--opened');
+      folder && folder.classList.toggle('folder--opened');
+
+      target.textContent = target.textContent === '+' ? '-' : '+';
+    }
   });
 }
-
-
-
-// toggleTreeViews(myTreeView);
-
